f890459da5faad4b0d62c9b2849cbc3222b413b0
KunalCarpenter <117262340+KunalCarpenter@users.noreply.github.com>
2025-08-01 16:46:35 +0530

    google login improve

--- loginpage/src/components/Login/Login.tsx
+++ loginpage/src/components/Login/Login.tsx
@@ -4,6 +4,10 @@ import * as Yup from 'yup';
 import styles from './Login.module.scss';
 import backgroundImg from '../../assets/login-background.png';
 import { Link } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
+import { useGoogleLogin } from '@react-oauth/google';
+//import {GoogleLogin} from '@react-oauth/google';
+//import {jwtDecode} from 'jwt-decode';
 
 const LoginSchema = Yup.object({
   email: Yup.string()
@@ -15,7 +19,48 @@ const LoginSchema = Yup.object({
 });
 
 const Login: React.FC = () => {
-  return (
+  const googleAuth = useGoogleLogin({
+    onSuccess: handleGoogleAuthSuccess
+    
+  });
+  async function handleGoogleAuthSuccess(response: any) {
+    
+    const { access_token } = response;
+    console.log("Access Token:", access_token); 
+    const res = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    const data = await res.json();
+    console.log("Email:", data.email);
+    console.log("First Name:", data.given_name);
+    console.log("Last Name:", data.family_name);
+    
+    // const googleUserInfoResponse = await googleAuthUserInfoService(access_token);
+    
+    // if (googleUserInfoResponse?.status !== 200) {
+      //   return showCustomToast(toastTitles.ERROR, messages.errorWithGoogleAuth);
+      // }
+      
+      // const userInfo = googleUserInfoResponse.data;
+      // const params = {
+        //   email: userInfo.email,
+        //   password: userInfo.id,
+        //   authType: authTypes.GOOGLE_SSO
+        // };
+        
+        // userSigninMutation.mutate(params);
+        
+      }
+      
+      //const navigate = useNavigate();
+      //const handleGoogleLogin = useGoogleLogin({
+        //  onSuccess: () => {navigate('/dashboard')},
+        //  onError: () =>{console.log("Login failed")}
+        //});
+      return (
     <div className={styles.wrapper}>
       <div className={styles.leftPanel}>
         <h2 className={styles.title}>Welcome back !</h2>
@@ -25,8 +70,9 @@ const Login: React.FC = () => {
         <Formik
           initialValues={{ email: '', password: '' }}
           validationSchema={LoginSchema}
+          // onSubmit={() => {navigate('/dashboard');}}
           onSubmit={() => {}}
-        >
+          >
           {({ errors, touched }) => (
             <Form className={styles.form}>
             <div className={styles.formGroup}>
@@ -51,7 +97,14 @@ const Login: React.FC = () => {
               <button type="submit" className={styles.button}>Log In</button>
               <p className={styles.orText}>or</p>
               <div className={styles.googleWrapper}>
-                <button type="button" className={styles.googleBtn}>
+                {/* <GoogleLogin 
+                  onSuccess={(credentialResponse) => {
+                      console.log(credentialResponse);
+                      console.log(jwtDecode(credentialResponse.credential || ""));
+                      //navigate('/dashboard');
+                    }}
+                  onError={() => {console.log("Login Failed")}}/> */}
+                <button type="button" className={styles.googleBtn} onClick={() => googleAuth()}>
                     <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google icon" />
                     <span>Login with Google</span>
                 </button>
